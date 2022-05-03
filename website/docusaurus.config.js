2545e6e0628a3379292be4bcd6d05126edb08abf
WeaponXII <29986707+WeaponXII@users.noreply.github.com>
2022-05-03 14:31:46 -0700

    Completed NEAR wiki edits.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,46 +1,44 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NEAR Wiki',
-  tagline: 'NEAR Wiki',
-  url: 'https://wiki.near.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'near', // Usually your GitHub org/user name.
-  projectName: 'wiki', // Usually your repo name.
-  scripts: [
-    "/js/hotjar.js",
-  ],
+  title: "NEAR Wiki",
+  tagline: "NEAR Wiki",
+  url: "https://wiki.near.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "near", // Usually your GitHub org/user name.
+  projectName: "wiki", // Usually your repo name.
+  scripts: ["/js/hotjar.js"],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          "showLastUpdateAuthor": true,
-          "showLastUpdateTime": true,
-          routeBasePath: '/',
-          "path": "../wiki",
-          sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          routeBasePath: "/",
+          path: "../wiki",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/near/wiki/edit/master/website',
+          editUrl: "https://github.com/near/wiki/edit/master/website",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
         },
         gtag: {
-          trackingID: 'G-Z976HN06X8',
+          trackingID: "G-Z976HN06X8",
           anonymizeIP: true,
         },
       }),
@@ -51,24 +49,24 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Wiki',
+        title: "Wiki",
         logo: {
-          alt: 'NEAR Logo',
-          src: 'img/near_logo.svg',
-          srcDark: 'img/near_logo_white.svg',
+          alt: "NEAR Logo",
+          src: "img/near_logo.svg",
+          srcDark: "img/near_logo_white.svg",
         },
         items: [
           {
-            href: '/',
-            position: 'left',
-            label: 'Getting Started',
+            href: "/",
+            position: "left",
+            label: "Getting Started",
           },
           {
-            href: '/ecosystem',
-            position: 'left',
-            label: 'Ecosystem',
+            href: "/ecosystem/dapps",
+            position: "left",
+            label: "Ecosystem",
           },
-          {
+          /*{
             href: '/technology',
             position: 'left',
             label: 'Technology',
@@ -82,71 +80,71 @@ const config = {
             href: '/community',
             position: 'left',
             label: 'Community',
-          },
+          },*/
           {
-            href: '/governance',
-            position: 'left',
-            label: 'Governance',
-          },
+            href: "/governance",
+            position: "left",
+            label: "Governance",
+          } /*
           {
             href: '/resources',
             position: 'left',
             label: 'Resources',
-          },
+          },*/,
           {
-            href: 'https://github.com/near/wiki',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/near/wiki",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Wiki',
-                to: '/',
+                label: "Wiki",
+                to: "/",
               },
               {
-                label: 'Dev Docs',
-                to: 'https://docs.near.org',
+                label: "Dev Docs",
+                to: "https://docs.near.org",
               },
               {
-                label: 'Specifications',
-                to: 'https://nomicon.io',
+                label: "Specifications",
+                to: "https://nomicon.io",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/nearprotocol',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/nearprotocol",
               },
               {
-                label: 'Discord',
-                href: 'https://near.chat',
+                label: "Discord",
+                href: "https://near.chat",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/NEARProtocol',
+                label: "Twitter",
+                href: "https://twitter.com/NEARProtocol",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'SDK Docs',
-                to: 'https://near-sdk.io/',
+                label: "SDK Docs",
+                to: "https://near-sdk.io/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/near/wiki',
+                label: "GitHub",
+                href: "https://github.com/near/wiki",
               },
             ],
           },
@@ -158,18 +156,18 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       algolia: {
-          // The application ID provided by Algolia
-          appId: 'J7VARGY7RM',
-          // Public API key: it is safe to commit it
-          apiKey: '4723f2a6e5ef0c7f5c6d17feb6362b19',
-          indexName: 'wiki-near',
-          // Optional: see doc section below
-          contextualSearch: true,
-          // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-          externalUrlRegex: 'near\\.org|nomicon\\.io',
-          // Optional: Algolia search parameters
-          searchParameters: {},
-          //... other Algolia params
+        // The application ID provided by Algolia
+        appId: "J7VARGY7RM",
+        // Public API key: it is safe to commit it
+        apiKey: "4723f2a6e5ef0c7f5c6d17feb6362b19",
+        indexName: "wiki-near",
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "near\\.org|nomicon\\.io",
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        //... other Algolia params
       },
     }),
 };
